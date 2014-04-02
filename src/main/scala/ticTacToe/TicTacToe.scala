fc1dc2baef5b3016773a57e906502ba8b4fefde9
Jose Abelardo Gutierrez <jose.abelardo@admios-sa.com>
2014-04-02 13:22:56 -0500

    pass test for board should start as a blank 3x3 array

---
+++ src/main/scala/ticTacToe/TicTacToe.scala
@@ -0,0 +1,14 @@
+package ticTacToe
+/* 
+ * Write a game that will take a tic-tac-toe board with X, O, and blank 
+ * characters and detect the winner or whether there is a tie or no winner yet.
+ * Use classes where appropriate.
+ *
+ * Bonus problem: Let two players play tic-tac-toe.
+ */
+ 
+
+
+class TicTacToe {
+  var board = Array.fill[Char](3, 3){' '} 
+}
